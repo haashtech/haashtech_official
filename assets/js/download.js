394e75bcc21aea934159068d9d0bd7493d2d1125
Muhammed Safvan kv <mskvphed@gmail.com>
2024-05-06 12:04:51 +0530

    change acted after completed

--- assets/js/download.js
+++ assets/js/download.js
@@ -14,3 +14,49 @@ document.getElementById('downloadButton').addEventListener('click', function() {
     // Trigger a click event on the anchor element to start the download
     link.click();
   });
+
+
+  // ============================
+  // Get the current URL
+const currentUrl = window.location.href;
+
+// Get all nav-links
+const navLinks = document.querySelectorAll('.nav-link');
+
+// Loop through each nav-link
+navLinks.forEach(navLink => {
+  // Get the href attribute of the nav-link
+  const href = navLink.getAttribute('href');
+
+  // Check if the current URL contains the href
+  if (currentUrl.includes(href)) {
+    // Add the 'active' class to the nav-link
+    navLink.classList.add('active');
+  }
+});
+
+// ===============================
+$(window).scroll(function () {
+  const navbar = $(".navbar");
+
+  // Add glass effect to navbar when scrolling
+  if ($(window).scrollTop()) {
+    navbar.addClass("glass");
+  } else {
+    navbar.removeClass("glass");
+  }
+});
+
+$("#offcanvasNavbar").on("show.bs.offcanvas", function () {
+  const navbar = $(".navbar");
+  navbar.removeClass("glass");
+});
+
+// document.addEventListener('scroll',()=>{
+//   const nav = document.querySelector('nav');
+//   if(window.scrollY > 0){
+//     nav.classList.add('scrolled')
+//   } else {
+//     nav.classList.remove('scrolled')
+//   }
+// })
